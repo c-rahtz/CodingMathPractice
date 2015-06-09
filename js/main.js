b8e022c54aa4da9c3409a777a3fde428c6a7c1d3
c-rahtz <craig.rahtz@hobsons.com>
2015-06-09 17:18:55 -0400

    splitting off 3d script and other updates

--- js/main.js
+++ js/main.js
@@ -1,26 +1,8 @@
-function init3d() {
-	console.log("init3d");
-	var scene = new THREE.Scene(),
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
-		renderer = new THREE.WebGLRenderer(),
-		geometry = new THREE.BoxGeometry( 1, 1, 1),
-		material = new THREE.MeshBasicMaterial( {color: 0xab2222}),
-		cube = new THREE.Mesh(geometry, material);
 
-
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild(renderer.domElement);
-
-		scene.add(cube);
-		camera.position.z = -5;
-
-}
 window.onload = function() {
-	var cvas = document.getElementById('canvas'),
-	context = canvas.getContext("2d"),
-	width = canvas.width = window.innerWidth,
-	height = canvas.height = window.innerHeight;
-	context.fillRect(0,0, width, height);
-
-	init3d();
+    var cvas = document.getElementById('canvas'),
+        context = canvas.getContext("2d"),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight;
+    	context.fillRect(0, 0, width, height);
 }
---
+++ js/three3Dev.js
@@ -0,0 +1,38 @@
+function init3d() {
+    console.log("init3d");
+    var scene = new THREE.Scene(),
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+        renderer = new THREE.WebGLRenderer(),
+        geometry = new THREE.BoxGeometry(1, 1, 1),
+        material = new THREE.MeshBasicMaterial({
+            color: 0xab2222
+        }),
+        cube = new THREE.Mesh(geometry, material),
+
+        render = function() {
+            console.log("render, render, render...");
+            requestAnimationFrame(render);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        }
+
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    scene.add(cube);
+    camera.position.z = 5;
+
+    render();
+
+}
+window.onload = function() {
+    // var cvas = document.getElementById('canvas'),
+    //     context = canvas.getContext("2d"),
+    //     width = canvas.width = window.innerWidth,
+    //     height = canvas.height = window.innerHeight;
+    // 	context.fillRect(0, 0, width, height);
+
+    init3d();
+}
